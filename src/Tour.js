32556e08d0dfbba2015710b7ffa0dc8db4302576
Sachin <skpkorba9009@gmail.com>
2022-10-05 19:39:40 +0530

    some project works

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+    const [readmore, setReadmore] = useState(false);
+
+    return (
+        <article className='single-tour'>
+            <img src={image} alt={name} />
+            <footer>
+                <div className='tour-info'>
+                    <h4>{name}</h4>
+                    <h4 className='tour-price'>Rs.{price}</h4>
+                </div>
+                <p>
+                    {readmore ? info : `${info.substring(0, 150)}...`}
+                    <span onClick={() => setReadmore(!readmore)}>{readmore ? ' Read less' : ' Read more'}</span>
+                </p>
+                <button className='delete-btn' onClick={() => removeTour(id)}>
+                    not interested
+                </button>
+            </footer>
+        </article>
+    );
 };
 
 export default Tour;
